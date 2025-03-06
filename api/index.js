2426e158ac587f3a4b25812bc196923fdd6a3934
Deeksha_GitFiles <sharmadeekksha@gmail.com>
2025-03-06 15:48:49 +0530

    updated vercel.json

--- api/index.js
+++ api/index.js
@@ -1,4 +1,4 @@
 const serverless = require("serverless-http");
-const app = require("../index"); // Adjust the path if needed
+const app = require("../index");
 
 module.exports.handler = serverless(app);
