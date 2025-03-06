d1651aceef92890d757b8808d8a4a82c7aacdd41
Deeksha_GitFiles <sharmadeekksha@gmail.com>
2025-03-06 19:22:11 +0530

    recheck

--- config/dbConnection.js
+++ config/dbConnection.js
@@ -1,15 +1,12 @@
 const mongoose = require("mongoose");
 
 const dbConnection = async () => {
-  const connect = await mongoose
-    .connect(process.env.AMAZON_CONNECTION_STRING)
-    .then(() => {
-      console.log("Mongodb Db connection success");
-    })
-    .catch((err) => {
-      console.log("Mongodb Db connection failed", err);
-    });
-    
+  try {
+    await mongoose.connect(process.env.AMAZON_CONNECTION_STRING);
+    console.log("MongoDB connection successful!");
+  } catch (err) {
+    console.log("MongoDB connection failed:", err);
+  }
 };
 
 module.exports = dbConnection;
