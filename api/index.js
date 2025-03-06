aa4e652d9c3976bea72ec06b575eaa9bab896b42
Deeksha_GitFiles <sharmadeekksha@gmail.com>
2025-03-06 22:23:58 +0530

    removing serverless

--- api/index.js
+++
@@ -1,4 +0,0 @@
-const serverless = require("serverless-http");
-const app = require("../index");
-console.log("Inside Serverless functions");
-module.exports = serverless(app);
--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 require("dotenv").config();
-const dbConnection = require("./config/DbConnection");
+const dbConnection = require("./config/dbConnection");
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
 const router = require("./routes/index.routes");
